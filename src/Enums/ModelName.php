ed2b37ba71c1d8305f7a2a321e7752504eea2a46
Ara Sivaneswaran <gohabs4ever@gmail.com>
2025-02-13 19:18:23 -0500

    update flash model

--- src/Enums/ModelName.php
+++ src/Enums/ModelName.php
@@ -9,7 +9,7 @@ enum ModelName: string
     case Default = 'models/text-bison-001';
     case GeminiPro = 'models/gemini-1.5-pro';
     case GeminiProExp = 'models/gemini-2.0-pro-exp-02-05';
-    case GeminiFlash = 'models/gemini-1.5-flash';
+    case GeminiFlash = 'models/gemini-2.0-flash';
     case GeminiFlashExp = 'models/gemini-2.0-flash-exp';
     case GeminiProVision = 'models/gemini-pro-vision';
     case Embedding = 'models/embedding-001';
